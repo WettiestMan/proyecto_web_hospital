bf9b4dce6d387e2c283426cdce71d9053a7f74ab
WettiestMan <S_4r8ol@protonmail.com>
2024-06-05 01:04:06 -0500

    mini modificaciones para mostrar solo lo que usé

--- api/routes/api.php
+++ api/routes/api.php
@@ -4,12 +4,9 @@ use App\Http\Controllers\FarmacosController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+/*Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
-/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});*/
+})->middleware('auth:sanctum');*/
 
 Route::get('farmacos', [FarmacosController::class, 'index'])->name('farmacos.index');
 Route::get('farmacos/{id}', [FarmacosController::class, 'show'])->name('farmacos.show');
