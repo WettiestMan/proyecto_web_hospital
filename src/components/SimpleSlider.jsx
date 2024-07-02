83abbedff17e417b794fe19411bc9064a70881b9
Paltita715 <manuel715pl@gmail.com>
2024-07-01 19:06:16 -0500

    usando api de carruselImagenes

--- src/components/SimpleSlider.jsx
+++ src/components/SimpleSlider.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import { Swiper, SwiperSlide } from "swiper/react"
 import { Autoplay, EffectFade, Pagination, Navigation } from 'swiper/modules'
-import { getPublicaciones } from '@/services/postService'
+import { getCarouselImages } from '@/services/postService'
 
 import 'swiper/css'
 import 'swiper/css/pagination'
@@ -9,15 +9,15 @@ import 'swiper/css/effect-fade'
 import 'swiper/css/navigation'
 
 const SimpleSlider = () => {
-  const [publis, setPublis] = useState([])
+  const [imgs, setImgs] = useState([])
 
-  const getAllPublicaciones = async () => {
-    const { publicaciones } = await getPublicaciones();
-    setPublis(publicaciones);
+  const getAllImagenes = async () => {
+    const { imagenes } = await getCarouselImages()
+    setImgs(imagenes)
   }
 
   useEffect(() => {
-    getAllPublicaciones()
+    getAllImagenes()
   }, [])
 
   return(
@@ -38,9 +38,9 @@ const SimpleSlider = () => {
         className="rounded-xl h-96"
       >
         {
-          publis.map(publi => (
+          imgs.map(img => (
             <SwiperSlide className="bg-slate-900">
-              <img src={`http://localhost:8000/storage/${publi.imagen}`} alt={publi.imagen} className="h-96 mx-auto"/>
+              <img src={`http://localhost:8000/storage/${img.imagen}`} alt={img.alt} className="h-96 mx-auto"/>
             </SwiperSlide>
           ))
         }
@@ -49,42 +49,3 @@ const SimpleSlider = () => {
 }
 
 export default SimpleSlider
-
-// import { useEffect, useState } from "react"
-// import { register } from 'swiper/element/bundle';
-// import { getPublicaciones } from '@/services/postService'
-
-// register();
-
-// const SimpleSlider = () => {
-//   const [publis, setPublis] = useState([])
-
-//   const getAllPublicaciones = async () => {
-//     const { publicaciones } = await getPublicaciones();
-//     setPublis(publicaciones);
-//   }
-
-//   useEffect(() => {
-//     getAllPublicaciones()
-//   }, [])
-
-//   return (
-//     <swiper-container
-//       slides-per-view="1"
-//       navigation="true"
-//       pagination="true"
-//       loop="true"
-//       class='h-96'
-//     >
-//       {
-//           publis.map(publi => (
-//             <swiper-slide>
-//               <img src={`http://localhost:8000/storage/${publi.imagen}`} alt={publi.imagen}/>
-//             </swiper-slide>
-//           ))
-//       }
-//     </swiper-container>
-//   )
-// }
-
-// export default SimpleSlider
