e88ec15e822abb4ab5c6f256086ed601dc1dad53
Paltita715 <manuel715pl@gmail.com>
2024-07-03 16:00:48 -0500

    Carrusel dinamico 100%

--- src/components/CarruselAdmin.jsx
+++ src/components/CarruselAdmin.jsx
@@ -1,23 +1,23 @@
-import { useEffect, useState } from "react"
-import { Swiper, SwiperSlide } from "swiper/react"
-import { Autoplay, EffectFade, Pagination, Navigation } from 'swiper/modules'
 import { getCarouselImages, deleteCarouselImage } from '@/services/postService'
+import { useEffect, useState, useRef } from "react"
+import { register } from 'swiper/element/bundle'
 import CarruselForm from "./CarruselForm"
 
-import 'swiper/css'
-import 'swiper/css/pagination'
-import 'swiper/css/effect-fade'
-import 'swiper/css/navigation'
+register()
 
 const CarruselAdmin = () => {
     const [imgs, setImgs] = useState([])
+    const swiperRef = new useRef(null)
 
     const getAllImagenes = async () => {
         const { imagenes } = await getCarouselImages()
         setImgs(imagenes)
+        if (swiperRef.current) {
+            swiperRef.current.swiper.update()
+        }
     }
 
-    const handleDelete = async (id) => {
+    const handleDelete =  async (id) => {
         if (window.confirm("¿Estas seguro de eliminar esta imagen?")) {
             await deleteCarouselImage(id)
             getAllImagenes()
@@ -27,8 +27,8 @@ const CarruselAdmin = () => {
     useEffect(() => {
         getAllImagenes()
     }, [])
-    
-    return(
+
+    return (
         <>
         <div className="relative flex py-4 items-center">
             <h2 className="absolute left-1/2 transform -translate-x-1/2 text-center font-black text-2xl uppercase">Administrador de Imagenes del Carrusel</h2>
@@ -36,47 +36,29 @@ const CarruselAdmin = () => {
                 <CarruselForm getAllImagenes={getAllImagenes}/>
             </div>
         </div>
-        <Swiper
-            slidesPerView={3}  //Slides visibles
-            spaceBetween={30}
-            // effect="fade"
-            // loop={true}  --> Da una advertencia :v
-            autoplay={{
-                delay: 5000,
-                disableOnInteraction: false,
-            }}
-            pagination={{
-                clickable: true,
-            }}
-            navigation={true}
-            modules={[Pagination, Autoplay, Navigation]}
-            className="rounded-xl h-96"
-        >
-            {/* Slides permanentes aqui */}
-            <SwiperSlide className="bg-slate-900">
-                <img src="/img/Inicio/slide1.jpg" alt="slide1" className="h-96 mx-auto"/>
-            </SwiperSlide>
-            <SwiperSlide className="bg-slate-900">
-                <img src="/img/Inicio/slide2.jpg" alt="slide2" className="h-96 mx-auto"/>
-            </SwiperSlide>
-            <SwiperSlide className="bg-slate-900">
-                <img src="/img/Inicio/slide3.jpg" alt="slide3" className="h-96 mx-auto"/>
-            </SwiperSlide>
-            <SwiperSlide className="bg-slate-900">
-                <img src="/img/Inicio/slide4.jpg" alt="slide4" className="h-96 mx-auto"/>
-            </SwiperSlide>
-        {
-            // Slides Dinamicos aqui 
-            // onClick={handleDelete(img.id)} --> en el boton de eliminar
-            imgs.map(img => (
-                <SwiperSlide key={img.id} className="bg-slate-900 relative">
-                    <button className="absolute top-0 right-0 bg-red-500 py-1 px-2 text-white">Eliminar</button>
-                    <img src={`http://localhost:8000/storage/${img.imagen}`} alt={img.alt} className="h-96 mx-auto"/>
-                </SwiperSlide>
-            ))
-        }
-      </Swiper>
-      </>
+        <swiper-container ref={swiperRef} class="h-[27rem]" pagination="true" pagination-clickable="true" pagination-dynamic-bullets="true" slides-per-view="auto" centered-slides="true" space-between="30" loop="true" autoplay-delay="10000" autoplay-disable-on-interaction="false">
+            <swiper-slide class="text-center flex justify-center items-center w-fit">
+                <img className='h-[27rem]' src="/img/Inicio/slide1.jpg" alt="slide1" />
+            </swiper-slide>
+            <swiper-slide class="text-center flex justify-center items-center w-fit">
+                <img className='h-[27rem]' src="/img/Inicio/slide2.jpg" alt="slide2" />
+            </swiper-slide>
+            <swiper-slide class="text-center flex justify-center items-center w-fit">
+                <img className='h-[27rem]' src="/img/Inicio/slide3.jpg" alt="slide3" />
+            </swiper-slide>
+            <swiper-slide class="text-center flex justify-center items-center w-fit">
+                <img className='h-[27rem]' src="/img/Inicio/slide4.jpg" alt="slide4" />
+            </swiper-slide>
+            {
+                imgs.map(img => (
+                    <swiper-slide class="text-center flex justify-center items-center w-fit relative">
+                        <button className='absolute top-0 right-0 bg-red-500 py-2 px-3' onClick={() => handleDelete(img.id)}>Borrar imagen</button>
+                        <img src={`http://localhost:8000/storage/${img.imagen}`} alt={img.alt} className="h-[27rem]"/>
+                    </swiper-slide>
+                ))
+            }
+        </swiper-container>
+        </>
     )
 }
 
