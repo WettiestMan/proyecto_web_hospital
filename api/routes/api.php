9cae1c3ac80700302559e17f104ddd34496886f1
WettiestMan <S_4r8ol@protonmail.com>
2024-07-04 14:44:16 -0500

    login al 50%

--- api/routes/api.php
+++ api/routes/api.php
@@ -4,16 +4,23 @@ use App\Http\Controllers\FarmacosController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PublicacionesController;
+use App\Http\Controllers\UserController;
 
 /*Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');*/
 
+// Para los fármacos
 Route::get('farmacos', [FarmacosController::class, 'index'])->name('farmacos.index');
 Route::get('farmacos/{id}', [FarmacosController::class, 'show'])->name('farmacos.show');
 
+// Para las publicaciones
 Route::get('publicaciones', [PublicacionesController::class, 'index']);
 Route::get('publicaciones/{id}', [PublicacionesController::class, 'show']);
 Route::post('publicaciones', [PublicacionesController::class, 'store']);
 Route::put('publicacionesupdate/{id}', [PublicacionesController::class, 'update']);
 Route::delete('publicacionesdelete/{id}', [PublicacionesController::class, 'destroy']);
+
+// Para la autenticación en la página de publicaciones
+Route::post('auth-posts', [UserController::class, 'authenticate']);
+Route::post('new-user', [UserController::class, 'create']);
